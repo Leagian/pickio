47c9864fd82c98a86a9fd8cd592bceb06c0d6ef6
Leagian <lea.giangrosso@gmail.com>
2024-03-31 19:51:35 +0200

    Added modules and prisma schema postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
+import { ShareModule } from './share/share.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    UserModule,
+    PostModule,
+    CommentModule,
+    ShareModule,
+    BookmarkModule,
+  ],
 })
 export class AppModule {}
